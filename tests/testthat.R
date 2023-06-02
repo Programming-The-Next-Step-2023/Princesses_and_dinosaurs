976dd6d26b834bf5dc2b355f8db784aa5a54da6a
Marlies <123662873+MarliesRoozendaal@users.noreply.github.com>
2023-06-02 21:04:09 +0200

    attempt at test
    But it the function returns an error, even though it works in the shiny app!

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(shinygame)
+library(PrincessDino)
 
-test_check("shinygame")
+test_check("PrincessDino")
